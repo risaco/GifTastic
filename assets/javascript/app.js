a052d9f36c71c6c5ff59c3dfb94942e529063e3f
Rhia Cooper <Rhia Cooper>
2017-08-05 19:48:29 -0500

    finalized js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -12,135 +12,174 @@ $(document).ready(function() {
 
 	// generate array for button topics
 
-	var topics = ["olenna tyrell", "jon snow", "daenerys", "cersei", 
-	"starks", "lannisters", "missandei", "khal drogo", "unsullied", 
+	var topics = ["olenna tyrell", "jon snow", "daenerys", "cersei",
+	"starks", "lannisters", "missandei", "khal drogo", "unsullied",
 	"sansa", "arya", "littlefinger", "jamie lannister", "tyrion"];
 
-	var topic = 0;
-
-	var topicButton;
+	var topic = 0; // indicator for position of topic in topics array
 
 
+	makeButtons(topics); // calls function to dynamically create buttons
 
-	// for each topic in the array...
-	$(topics).each(function	() {
-
-		// create button for each topic in the array
-		topicButton = $("<button/>").addClass("btn btn-default").attr("id", "topicBtn").attr("name", topics[topic]);
-
-		// add the topic as the title of the button
-		$(topicButton).append(topics[topic]);
-
-		// append button to div id topicButtons
-		$("#topicButtons").append(topicButton);
-
-		console.log(topicButton);
-
-		topic++;
-	});
 
 	var characterName;
 
 	var queryURL;
 
-	// when a topic button is clicked...
-	$("#topicBtn").on("click", function() {
+	getGifs(); // calls function to get GIFs from Giphy API
+
+	$("#topicForm").on("click", "button", function() {
+
+		event.preventDefault();
+
+		// clear gifButtons div
+		$("#gifButtons").empty();
+
+		var addCharacter = $("input").val().trim();
+		console.log(addCharacter);
+
+		topics.push(addCharacter);
+		console.log(topics);
+
+		topic = 0;
+
+		// clear topicButtons div
+		$("#topicButtons").empty();
+
+		makeButtons(topics);
+
+		// clear text from form
+		$("input").val('');
+
+	})
+
+	// ***** FUNCTIONS *****
+		function makeButtons(array) {
+			// for each topic in the array...
+			$(array).each(function	() {
+
+				// create button for each topic in the array
+
+				// add class id and name attribute
+				var topicButton = $("<button/>").addClass("btn btn-default").attr("id", "topicBtn");
+				$(topicButton).attr("name", array[topic]);
+
+				// add the topic as the title of the button
+				$(topicButton).append(topics[topic]);
+
+				// append button to div id topicButtons
+				$("#topicButtons").append(topicButton);
+
+				console.log(topicButton);
+
+				topic++; // increment by one to move to next topic in array
+			});
+		} // end makeButtons function
+
+		function getGifs() {
+
+		// when a topic button is clicked...
+		$("#topicButtons").on("click","button", function() {
+
+			// clear the #gifButtons div
+			$("#gifButtons").empty();
+
+			// capture the character's name for this button
+			characterName = $(this).attr("name") + " game of thrones";
+
+			console.log(characterName);
+
+			// queryURL for Giphy API
+				queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+					characterName + "&api_key=11c9af6afb4a4da28efed9d8c79be813&limit=10";
+
+				$.ajax({
+					url: queryURL,
+					method: 'GET'
+				}).done(function(response) {
+						console.log(response);
+
+						console.log(response.data);
+
+						// console.log(response.data[0].images.fixed_height_still.url);
+
+						// for each item in the .data...
+						for (i = 0; i < (response.data).length; i++){
+
+							// create a var to hold the fixed_height animated url
+							var animateImage = response.data[i].images.fixed_height.url;
+
+							var stillImage = response.data[i].images.fixed_height_still.url;
+
+							var rating = response.data[i].rating;
+
+							// console.log("animated url: " + animateImage + " | still url: " + stillImage);
+
+							// create a new divs and rows to hold the rating and image
+
+							var divImage = $("<div/>").addClass("col-md-3");
+
+							var pRow = $("<div/>").addClass("row").attr("id", "pRow");
 
 
-		// capture the character's name for this button
-		characterName = $(this).attr("name");
+							var imgRow = $("<div/>").addClass("row").attr("id", "imgRow");
 
-		console.log(characterName);
+							$(divImage).append(pRow).append(imgRow);
 
-		// queryURL for Giphy API
-	    queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-        characterName + "&api_key=11c9af6afb4a4da28efed9d8c79be813&limit=10";
+							// create paragraph tag for rating and append to pRow
+							var mainTag = $("<p><h3>Rating: " + rating + "</h3></p>");
 
-	    $.ajax({
-	      url: queryURL,
-	      method: 'GET'
-	    }).done(function(response) {
-	      	console.log(response);
+							$(pRow).append(mainTag);
 
-	      	console.log(response.data);
+							// create a new var to hold an image tag with class gif
+							var gifImage = $("<img/>").addClass("gif");
 
-	      	// console.log(response.data[0].images.fixed_height_still.url);
+							// make src attribute equal to still url
+							$(gifImage).attr("src", stillImage);
 
-	      	// for each item in the .data...
-	      	for (i = 0; i < (response.data).length; i++){
+							// add attributes for still, animate, and state
 
-		      	// create a var to hold the fixed_height animated url
-		      	var animateImage = response.data[i].images.fixed_height.url;
+							$(gifImage).attr("data-still", stillImage).attr("data-animate", animateImage).attr("data-state", "still");
 
-		      	var stillImage = response.data[i].images.fixed_height_still.url;
-
-		      	var rating = response.data[i].rating;
-
-		      	// console.log("animated url: " + animateImage + " | still url: " + stillImage);
-
-		      	// create a new divs and rows to hold the rating and image
-
-		      	var divImage = $("<div/>").addClass("col-md-3");
-
-		      	var pRow = $("<div/>").addClass("row").attr("id", "pRow");
+							$(imgRow).append(gifImage);
 
 
-		      	var imgRow = $("<div/>").addClass("row").attr("id", "imgRow");
+						// append images to gifButtons div
+						$("#gifButtons").append(divImage);
 
-		      	$(divImage).append(pRow).append(imgRow);
+					} // end of for loop
 
-		      	// create paragraph tag for rating and append to pRow
-		      	var mainTag = $("<p><h3>Rating: " + rating + "</h3></p>");
+					// when a gif image is clicked...
+					$(".gif").on("click", function() {
 
-		      	$(pRow).append(mainTag);
+						var state = $(this).attr("data-state");
 
-		      	// create a new var to hold an image tag with class gif
-		      	var gifImage = $("<img/>").addClass("gif");
+						// if the state is "still"
+						if (state === "still") {
 
-		      	// make src attribute equal to still url
-		      	$(gifImage).attr("src", stillImage);
+							// set this items src to the animated url
+							$(this).attr("src", $(this).attr("data-animate"));
 
-		      	// add attributes for still, animate, and state
+							// set this item's data-state to "animated"
+							$(this).attr("data-state", "animated");
+						}
 
-		      	$(gifImage).attr("data-still", stillImage).attr("data-animate", animateImage).attr("data-state", "still");
-		    
-				$(imgRow).append(gifImage);
+						else {
 
+							$(this).attr("src", $(this).attr("data-still"));
 
-		    	// append images to gifButtons div
-		    	$("#gifButtons").append(divImage);
-
-		    }
-
-		    // when a gif image is clicked...
-		    $(".gif").on("click", function() {
-
-		    	var state = $(this).attr("data-state");
-
-		    	// if the state is "still"
-		    	if (state === "still") {
-
-		    		// set this items src to the animated url
-		    		$(this).attr("src", $(this).attr("data-animate"));
-
-		    		// set this item's data-state to "animated"
-		    		$(this).attr("data-state", "animated");
-		    	}
-
-		    	else {
-
-		    		$(this).attr("src", $(this).attr("data-still"));
-
-		    		$(this).attr("data-state", "still");
-		    	}
+							$(this).attr("data-state", "still");
+						}
 
 
-		    })
+					}); // end of gif on.click function
 
 
-	    });
+				}); // end of ajax.done function
 
-	    
 
-	});
-});
+		}); // end of button on.click function
+
+	} // end of getGifs function
+
+}); // end of script
